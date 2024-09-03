8ac6fac69c365bf5a92f3ae076f5082552ac69e8
Trevor Durán <trevor.m.duran@gmail.com>
2024-09-02 20:17:26 -0600

    feat: add database implementation

--- src/Database.ts
+++ src/Database.ts
@@ -1,22 +1,25 @@
 class Database implements iDatabase {
-
-    data;
+    #data: Map<string, Map<string, string>>;
 
     constructor() {
-        this.data = {};
+        this.#data = new Map<string, Map<string, string>>();
     }
 
-    set(id: string, key: string, val: string) {
-        throw new Error('Not implemented');
+    set(id: string, key: string, val: string): void {
+        if (!this.#data.has(key)) {
+            this.#data.set(id, new Map([[key, val]]));
+        }
+        this.#data.get(id)?.set(key, val);
     }
 
-    get(id: string, key: string) {
-        throw new Error('Not implemented');
-        return '';
+    get(id: string, key: string): string {
+        const returnVal = this.#data.get(id)?.get(key);
+        return returnVal ? returnVal : '';
     }
 
-    delete(id: string, key: string) {
-        throw new Error('Not implemented');
+    delete(id: string, key: string): void {
+        const delEntry = this.#data.get(id);
+        delEntry?.delete(key);
     }
 }
 
